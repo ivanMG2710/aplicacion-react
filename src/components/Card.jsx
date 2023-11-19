daa70bc6a37b07d5f772a5e504d282d8c1e5370c
ivanMG2710 <19240282@leon.tecnm.mx>
2023-11-19 12:12:34 -0600

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Card.css';
+
+const Card = ({ lang, img, fcolor, scolor}) => {
+
+    return (
+        //Agregando los estilos
+        <div 
+            className="card"
+            style={{background: `linear-gradient(to left, ${fcolor}, ${scolor})`}}>
+            <img src={img} alt="lang.svg" />
+            <h3>{lang}</h3>
+        </div>
+    );
+};
+
+//Definiendo los proptypes de Card
+Card.protoTypes = {
+    lang: PropTypes.string,
+    img: PropTypes.string,
+    fcolor: PropTypes.string,
+    scolor: PropTypes
+};
+
+export default Card;
